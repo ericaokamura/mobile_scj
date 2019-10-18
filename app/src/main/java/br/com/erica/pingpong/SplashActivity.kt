c4047966f137dcdc3af877acf9c7ea7a0e240fcd
Logon Aluno <logonaluno@local.com>
2019-10-17 21:41:11 -0300

    utilizando intent

--- app/src/main/java/br/com/erica/pingpong/SplashActivity.kt
+++ app/src/main/java/br/com/erica/pingpong/SplashActivity.kt
@@ -1,12 +1,25 @@
 package br.com.erica.pingpong
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
 
 class SplashActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash)
+
+        Handler().postDelayed({
+            nextScreen()
+        }, 2000L)
+
+    }
+
+    private fun nextScreen() {
+        var intent = Intent(this, MainActivity::class.java)
+        startActivity(intent)
+        this.finish()
     }
 }
